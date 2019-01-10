e862ae918908a42783cbfa80a54070cbdc3e58c1
andre <chang432@purdue.edu>
2019-01-10 17:14:33 -0500

    sup

--- tippy/SettingsViewController.swift
+++ tippy/SettingsViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var firstEdit: UITextField!
+    @IBOutlet var secondEdit: UITextField!
+    @IBOutlet var thirdEdit: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
 
+        let defaults = UserDefaults.standard
+        
+        firstEdit.text = String(format:"%.0f", defaults.double(forKey: "one") * 100)
+        secondEdit.text = String(format:"%.0f", defaults.double(forKey: "two") * 100)
+        thirdEdit.text = String(format:"%.0f", defaults.double(forKey: "three") * 100)
         // Do any additional setup after loading the view.
     }
 
@@ -20,9 +31,24 @@ class SettingsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+    //defaults.set(Double(Int()), forKey: )
+    
+    @IBAction func goBack(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        defaults.set(Double(firstEdit.text!)!/100.0, forKey: "one")
+        defaults.set(Double(secondEdit.text!)!/100.0, forKey: "two")
+        defaults.set(Double(thirdEdit.text!)!/100.0, forKey: "three")
+        
+        navigationController?.popViewController(animated: true)
+    }
+    
+/*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -30,6 +56,6 @@ class SettingsViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+ */
 
 }
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet var billField: UITextField!
@@ -17,7 +18,25 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let control = BetterSegmentedControl(
+            frame: CGRect(x: 0, y: 0, width: 300, height: 44),
+            segments: LabelSegment.segments(withTitles: ["One", "Two", "Three"],
+                                            normalFont: UIFont(name: "HelveticaNeue-Light", size: 14.0)!,
+                                            normalTextColor: .lightGray,
+                                            selectedFont: UIFont(name: "HelveticaNeue-Bold", size: 14.0)!,
+                                            selectedTextColor: .white),
+            index: 1,
+            options: [.backgroundColor(.darkGray),
+                      .indicatorViewBackgroundColor(.blue)])
+        control.addTarget(self, action: #selector(ViewController.controlValueChanged(_:)), for: .valueChanged)
+        view.addSubview(control)
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let defaults = UserDefaults.standard
+        defaults.set(0.18, forKey: "one")
+        defaults.set(0.2, forKey: "two")
+        defaults.set(0.25, forKey: "three")
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +44,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        let defaults = UserDefaults.standard
+        
+        let one = defaults.double(forKey: "one")
+        let two = defaults.double(forKey: "two")
+        let three = defaults.double(forKey: "three")
+        
+        //let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let oneStr:String = String(format:"%.0f", one * 100)
+        let twoStr:String = String(format:"%.0f", two * 100)
+        let threeStr:String = String(format:"%.0f", three * 100)
+        
+        tipControl.setTitle(oneStr+"%", forSegmentAt: 0)
+        tipControl.setTitle(twoStr+"%", forSegmentAt: 1)
+        tipControl.setTitle(threeStr+"%", forSegmentAt: 2)
+        
+        let tipPercentages = [one, two, three]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+    
     @IBAction func onTap(_ sender: Any) {
         view.endEditing(true)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
-        let tipPercentages = [0.18, 0.2, 0.25]
+        self.reloadInputViews()
+        
+        let defaults = UserDefaults.standard
+        
+        let one = defaults.double(forKey: "one")
+        let two = defaults.double(forKey: "two")
+        let three = defaults.double(forKey: "three")
+        
+        //let tipPercentages = [0.18, 0.2, 0.25]
+        let tipPercentages = [one, two, three]
+        
+        let oneStr = String(format:"%.0f", one * 100)
+        let twoStr:String = String(format:"%.0f", two * 100)
+        let threeStr:String = String(format:"%.0f", three * 100)
+
+        tipControl.setTitle(oneStr+"%", forSegmentAt: 0)
+        tipControl.setTitle(twoStr+"%", forSegmentAt: 1)
+        tipControl.setTitle(threeStr+"%", forSegmentAt: 2)
         
         let bill = Double(billField.text!) ?? 0
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
